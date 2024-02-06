53ce4dd8864f09546afe2dd8a45ac049d63f30cb
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-02-06 23:03:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header class="wrapper backgroundPic">
+    <div>
+      <HelloWorld />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  aspect-ratio: 16/9;
+}
+
+main {
+  padding: 2rem;
+}
+
+.backgroundPic {
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  background-repeat: no-repeat;
+
+}
+.backgroundPic::before {
+  background-image: url("src/assets/Tezza-8603.jpg");
+	background-size: cover;
+	position: absolute;
+	top: 0px;
+	right: 0px;
+	bottom: 0px;
+	left: 0px;
+	opacity: 0.6;
+	content: "";
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="stalemate">
+      Gedli & Siim
+    </h1>
+    <h3>
+      27.07.2024
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 4rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  position: relative;
+}
+
+.stalemate {
+  font-family: 'Stalemate';
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  h1 {
+  font-weight: 500;
+  font-size: 5rem;
+}
+
+h3 {
+  font-size: 2rem;
+  position: relative;
+}
+  .greetings h1,
+  
+  .greetings h3 {
+    text-align: center;
+   
+  }
+}
+</style>
